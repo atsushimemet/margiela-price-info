4588cfcb7e20577a3b26acdad399ef1fa79b49c9
atsushimemet <noap3b69n@gmail.com>
2023-06-15 09:32:37 +0900

    Update create_insta_feed

--- create_insta_feed.py
+++ create_insta_feed.py
@@ -1,49 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
 import os
 from pathlib import Path
+import argparse
+from logzero import logger
 
 
-# In[2]:
+def main(yyyymmdd: str, brand: str):
+    file_list = [f for f in os.listdir(f"./data/output/{brand}") if yyyymmdd in f]
+    price_list = []
+    for file in file_list:
+        with open(Path(f"data/output/{brand}") / file, "r") as f:
+            price_list.append(int(f.read().split("価格: ")[1].split("\n")[0]))
+    logger.info(f"平均値: {round(sum(price_list) / len(price_list))}")
+    logger.info(f"最安値: {min(price_list)}")
+    logger.info(f"最高値: {max(price_list)}")
 
 
-brand = "margiela"
-yyyymmdd = "20230614"
+def arg_parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("yyyymmdd", type=str)
+    parser.add_argument("brand", type=str)
+    return parser.parse_args()
 
 
-# In[3]:
-
-
-file_list = [f for f in os.listdir(f"./data/output/{brand}") if yyyymmdd in f]
-
-
-# In[6]:
-
-
-price_list = []
-for file in file_list:
-    with open(Path(f"data/output/{brand}") / file, "r") as f:
-        price_list.append(int(f.read().split("価格: ")[1].split("\n")[0]))
-
-
-# In[7]:
-
-
-round(sum(price_list) / len(price_list))
-
-
-# In[8]:
-
-
-min(price_list)
-
-
-# In[9]:
-
-
-max(price_list)
-
+if __name__ == "__main__":
+    args = arg_parse()
+    main(args.yyyymmdd, args.brand)
