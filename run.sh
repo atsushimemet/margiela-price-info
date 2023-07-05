49e57352653e7746a0840116c640d11014a8b1a0
atsushimemet <noap3b69n@gmail.com>
2023-07-06 07:37:01 +0900

    Update run.shのget_product_infoの引数にitemを追加

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
-brand="margiela"
+brand="vuitton"
+item="財布"
 date=$(date +%Y%m%d)
-python get_product_info.py $brand
+python get_product_info.py $brand $item
 python auto_post_margiela.py $date $brand
 python create_insta_feed.py $date $brand
