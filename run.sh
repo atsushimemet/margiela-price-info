4b3cf491684c202d84abc7560cecf151a3e9ad2e
atsushimemet <noap3b69n@gmail.com>
2024-05-02 03:40:39 +0900

    ログの出力とcronの調整 (#10)
    * update: start_date init
    
    * update: ignore log.txt
    
    * update: log output
    
    * update: cron available
    
    * update: common path & log start and end time

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 #!/bin/zsh
-# CSVファイルのパス
-csv_file="data/input/arisa_brand_item_model.csv"
+# run.sh
+
+# Base directory
+BASE_DIR="/Users/ozawaatsushi/Repository/NewBusiness/margiela-price-info"
+
+log_file="$BASE_DIR/log.txt"
+csv_file="$BASE_DIR/data/input/arisa_brand_item_model.csv"
+counter_file="$BASE_DIR/.counter"
 
 # カウンターを初期化
-counter_file=".counter"
 if [ ! -f "$counter_file" ]; then
     echo "1" > "$counter_file"
+    echo "$(date): Counter initialized to 1." >> $log_file
 fi
 
 # カウンターを読み込む
@@ -23,6 +29,7 @@ if [ -z "$csv_line" ]; then
     # カウンターがCSVの行数を超えていた場合は最初の行に戻す
     csv_line=$(get_csv_line "1")
     echo "1" > "$counter_file"
+    echo "$(date): Counter exceeded line count, reset to 1." >> $log_file
 else
     # カウンターをインクリメント
     counter=$((counter + 1))
@@ -33,15 +40,13 @@ fi
 brand=$(echo "$csv_line" | cut -d',' -f2)
 item=$(echo "$csv_line" | cut -d',' -f3)
 model=$(echo "$csv_line" | cut -d',' -f4)
-
-# item変数にmodelを追加する
 item="$item $model"
-echo $brand
-echo $item
+echo "$(date): Processing item: $brand, $item" >> $log_file
 
 # 日付を取得
 date=$(date +%Y%m%d)
 
 # Pythonスクリプトを実行
-python get_product_info.py "$brand" "$item"
-python auto_post_margiela.py "$date" "$brand"
+PYTHON="/Users/ozawaatsushi/.pyenv/versions/3.10.5/bin/python"
+$PYTHON $BASE_DIR/get_product_info.py "$brand" "$item"
+$PYTHON $BASE_DIR/auto_post_margiela.py "$date" "$brand"
--- run_every_other_day.sh
+++ run_every_other_day.sh
@@ -1,6 +1,16 @@
 #!/bin/zsh
 # run_every_other_day.sh
-start_date="2024-05-01"
+
+# Base directory
+BASE_DIR="/Users/ozawaatsushi/Repository/NewBusiness/margiela-price-info"
+
+log_file="$BASE_DIR/log.txt"
+
+# Log the start time
+start_time=$(date '+%Y-%m-%d %H:%M:%S')
+echo "Script started at: $start_time" >> $log_file
+
+start_date="2024-05-02"
 current_date=$(date +%Y-%m-%d)
 
 # 日付を YYYY-MM-DD 形式で扱い、日数の差を計算 (macOS用)
@@ -10,8 +20,12 @@ days_difference=$(( ($current_seconds - $start_seconds) / 86400 ))
 
 # 経過日数が偶数の場合にスクリプトを実行
 if [ $((days_difference % 2)) -eq 0 ]; then
-    echo "Script is running on : current_date:$current_date, days_difference:$days_difference"
-    ./run.sh
+    echo "Script is running on : current_date:$current_date, days_difference:$days_difference" >> $log_file
+    $BASE_DIR/run.sh >> $log_file 2>&1
 else
-    echo "Script is not running on : current_date:$current_date, days_difference:$days_difference"
+    echo "Script is not running on : current_date:$current_date, days_difference:$days_difference" >> $log_file
 fi
+
+# Log the end time
+end_time=$(date '+%Y-%m-%d %H:%M:%S')
+echo "Script ended at: $end_time" >> $log_file
