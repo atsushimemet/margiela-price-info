0ef3eb71575100d989846b9994717c5d1486b29c
atsushimemet <noap3b69n@gmail.com>
2024-12-30 15:01:25 +0900

    refactor: mv settings (#18)
    * refactor: mv settings
    
    * fix: initial status

--- src/config.py
+++ src/config.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 from typing import Dict, Union
 
+import tweepy
+
 from src.local_config import CLIENT_ME, TwitterAPI  # noqa
 
+# 楽天
 REQ_URL = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
 MAX_PAGE = 10  # NOTE: auto_postが300ほどしかできない。
 HITS_PER_PAGE = 30
-
-
 REQ_PARAMS: Dict[str, Union[str, int, Dict[str, str]]] = {
     "applicationId": CLIENT_ME["APPLICATION_ID"],
     "affiliateId": CLIENT_ME["AFF_ID"],
@@ -25,5 +26,16 @@ WANT_ITEMS = [
     "itemUrl",
 ]
 
-PATH_OUTPUT_DIR = Path("./data/output/")
+# Twitter
 DAILY_FREE_TWEET_LIMIT = 50
+client = tweepy.Client(
+    bearer_token=TwitterAPI.BearerToken,
+    consumer_key=TwitterAPI.Key,
+    consumer_secret=TwitterAPI.KeySecret,
+    access_token=TwitterAPI.AccessToken,
+    access_token_secret=TwitterAPI.AccessTokenSecret,
+)
+
+# その他
+BASE_DIR = "./"
+PATH_OUTPUT_DIR = Path("./data/output/")
